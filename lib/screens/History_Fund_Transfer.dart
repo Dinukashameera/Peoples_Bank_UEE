a46f005fc5248410f78ab20ba29dd98f74753404
dinuka shameera <dinukashameera0657@gmail.com>
2020-10-06 23:48:55 +0530

    history screen added

--- lib/screens/History_Fund_Transfer.dart
+++ lib/screens/History_Fund_Transfer.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-
+import '../widgets//history_items.dart';
 class HistoryFundTransfer extends StatelessWidget {
+  final List transactions;
+  HistoryFundTransfer(this.transactions);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Fund transfer'),
-      ),
-    );
+        child: ListView(
+      children: transactions.map((item) => HistoryItems(item)).toList(),
+    ));
   }
 }
